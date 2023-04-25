eb91e539380d499aca47d62260cc55720ab8638c
PrajwolM <p.mdr79@gmail.com>
2023-04-25 12:04:44 +0545

    frontal design completion

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -3,17 +3,16 @@ import React from 'react';
 import './App.css';
 import WeatherForecast from './components/WeatherForecast';
 
-const api={
-  key: 'ba6d7b60dd9d3f68e6880210d2ab6eb8',
-  base: 'https://api.openweathermap.org/data/2.5/'
-}
+// const api={
+//   key: 'ba6d7b60dd9d3f68e6880210d2ab6eb8',
+//   base: 'https://api.openweathermap.org/data/2.5/'
+// }
 
 function App() {
   return (
-    <div className="App">
-      This is the App page.
-      <WeatherForecast/>
-    </div>
+    <>
+    <WeatherForecast/>
+    </>
   );
 }
 
