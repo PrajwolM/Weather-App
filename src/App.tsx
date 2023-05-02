5c61258ba7bec6f66f5af14323a09780880be3eb
PrajwolM <p.mdr79@gmail.com>
2023-05-02 10:42:48 +0545

    Garbage removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
 import React, { useState } from "react";
 
 import "./App.css";
-// import { ITempResp, ErrorHandle } from "./utils/type";
 import Homepage from "./components/Homepage";
 
 function App() {
-  // const [search, setSearch] = useState<string>("");
-  // const [situations, setSituation] = useState<ITempResp | null>();
-  // const [err, setErr] = useState<ErrorHandle | null>();
-  // const handleSearch = () => {
-  //   fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
-  //     .then(async (res) => await res.json())
-  //     .then((result) => {
-  //       if (result?.cod !== 200) {
-  //         setErr(result);
-  //         setSituation(null);
-  //       } else {
-  //         setSituation(() => result);
-  //         setErr(null);
-  //       }
-  //     });
-  // };
-
-  // const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   handleSearch();
-  // };
-  // const addLocation = () => {
-  //   locations.push(search);
-  //   console.log(locations);
-  // };
   const [places, setPlaces] = useState<string[]>([
     "kathmandu",
     "banepa",
@@ -69,53 +43,3 @@ function App() {
 }
 
 export default App;
-
-{
-  /* <div>
-          <Homepage />
-        </div>
-        <div className="w-full  md:max-w-[500px] p-4  text-center   lg:p-24 h-64 lg:h-[500px] bg-white bg-opacity-40 backdrop-blur-lg rounded drop-shadow-lg text-zinc-700">
-          <form onSubmit={handleForm}>
-            <input
-              className=" rounded-l-md py-1 px-2 rounded-1-md border-1 border-white"
-              type="text"
-              placeholder="Enter location"
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <button
-              className="py-1 px-2 rounded-r-md bg-transparent hover:bg-green-700 text-black font-semibold hover:text-white border border-black hover:border-transparent "
-              onClick={handleSearch}
-            >
-              Search
-            </button>
-          </form>
-
-          {situations && (
-            <div className=" text-xl">
-              <p className=" py-1 px-2  text-2xl">{situations?.name}</p>
-              <p className="relative left-10">
-                <img
-                  className="relative  left-[4rem]"
-                  src={`https://openweathermap.org/img/wn/${situations?.weather[0]?.icon}@2x.png`}
-                  alt="Weather Icon"
-                />
-              </p>
-              Temperature: <span> {situations?.main?.temp}</span>°C
-              <br />
-              Weather: <span> {situations?.weather[0]?.main}</span>
-              <p> {situations?.weather[0]?.description}</p>
-            </div>
-          )}
-          {err && (
-            <div>
-              {err.cod} Error: <span>{err.message}</span>
-            </div>
-          )}
-          <button
-            className="py-1 px-2 absolute bottom-2 right-2  rounded-md bg-transparent hover:bg-green-700 text-black font-semibold hover:text-white border border-black hover:border-transparent "
-            onClick={addLocation}
-          >
-            Add location
-          </button>
-        </div> */
-}
